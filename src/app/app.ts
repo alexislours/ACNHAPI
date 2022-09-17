b1da88d3f3a704e228054131269d9c45518b34c3
Surya Darma <budisuryadarma0407@gmail.com>
2022-09-17 15:45:56 +0800

    reconfig expres app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,9 @@
 import express, {Express} from 'express'
 
 const app: Express = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
 })
 
 export default app
