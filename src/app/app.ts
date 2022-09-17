2764f8613a404d1065a94e235ae9ad88cb685a92
Surya Darma <budisuryadarma0407@gmail.com>
2022-09-17 14:26:15 +0800

    init server

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import express, {Express} from 'express'
+
+const app: Express = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+export default app
